24b8648cde267c173a4f1ee870e85f92786357d3
levietloi <leloi171100@gmail.com>
2026-03-11 15:21:01 +0700

    update feature

---
+++ HotelManagement/Pages/Admin/Hotel.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelManagement.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ HotelManagement/Pages/Admin/ManageRooms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelManagement.Pages.Admin
+{
+    public class ManageRoomsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
