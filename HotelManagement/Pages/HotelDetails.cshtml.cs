5a12ee85fe823097805dbd16f40ddbd866cd6fc8
LeTrungKhai <letrungkhai20211@gmail.com>
2026-03-13 00:58:24 +0700

    update feature

---
+++ HotelManagement/Pages/HotelDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelManagement.Pages
+{
+    public class HotelDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HotelManagement/Pages/Hotels.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelManagement.Pages
+{
+    public class HotelsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
